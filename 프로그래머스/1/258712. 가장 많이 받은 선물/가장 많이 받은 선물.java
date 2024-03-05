07e33e4f864ed97811e280c8b6f062a08ee537b8
bbangHo <sonsins1@naver.com>
2024-03-06 01:58:58 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.43 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Friend {
+    int in;     // 선물 받음
+    int out;
+    
+    public Friend(int i, int o) {
+        in = i;
+        out = o;
+    }
+}
+
+class Solution {
+    static int[] ans;
+    static int[] point;
+    
+    public int solution(String[] friends, String[] gifts) {
+        /**
+        * 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 "다음 달"에 선물을 하나 받음
+        * 두 사람이 선물을 주고받은 "기록이 하나도 없거나" "주고받은 수가 같다면"
+        * 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람에게 선물을 하나 받습니다.
+        * '선물 지수'는 이번 달까지 자신이 친구들에게 준 선물 - 받은 선물의 수
+        * 선물 지수도 같다면 다음 달에 선물을 주고받지 않습니다.
+        * return 선물을 가장 많이 받을 친구가 받을 선물의 수
+        * ! 이름 = 알파벳 소문자로 이루어진 길이가 10 이하인 문자열
+        * ! 이름이 같은 친구는 없습니다. 
+        * ! A가 B에게 선물을 준다. A != B
+        */
+        int n = friends.length;
+        Map<String, Integer> map = new HashMap<>();  // 이름-인덱스 매핑
+        int[][] table = new int[n][n];  // 주고 받은 선물
+        point = new int[n];
+        
+        int idx = 0;
+        for(String f : friends) {
+            map.put(f, idx++);
+        }
+        
+        for(String g : gifts) {
+            String[] str = g.split(" ");
+            
+            int sender = map.get(str[0]);
+            int reciver = map.get(str[1]);
+            
+            table[sender][reciver]++;
+            point[sender]++;    // 준 선물 수 -
+            point[reciver]--;   // 받은 선물의 수
+        }
+        
+        ans = new int[n];
+        for(int sndr = 0; sndr < n; sndr++) {
+            for(int rcv = 0; rcv < n; rcv++) {
+                int sender = table[sndr][rcv];
+                int reciver = table[rcv][sndr];
+            
+                if(sndr != rcv) {
+                    if(sender == 0 && reciver == 0) {   // 주고 받은 기록이 하나도 없다면
+                        getGift(sndr, rcv);     // 선물 지수가 큰 사람이 받음
+                    } else if(sender > 0 || reciver > 0) { // 주고 받은 기록이 있다면
+                        if(sender == reciver) { // 주고 받은 수가 같다면
+                            getGift(sndr, rcv);
+                        } // 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 "다음 달"에 선물을 하나 받음
+                        else if(sender > reciver) {
+                            ans[sndr]++;
+                        } else {
+                            ans[rcv]++;
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+        Arrays.sort(ans);
+        
+        return ans[n-1] / 2;    // 위 로직에서 두 번씩 체크되기 때문에 2로 나눔
+    }
+    
+    public void getGift(int a, int b) {
+        if(point[a] > point[b]) {  
+            ans[a]++;
+        } else if(point[a] < point[b]) {
+            ans[b]++;
+        }
+    }
+}
