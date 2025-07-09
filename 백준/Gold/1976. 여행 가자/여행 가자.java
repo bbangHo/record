d368926698cc9dac26b2d364439f642434760aeb
bbangHo <sonsins1@naver.com>
2025-07-09 11:37:18 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 17060 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -8,52 +8,53 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        List<Integer>[] cities = new ArrayList[N + 1];
+        int[] unf = new int[N + 1];
+        for(int i = 1; i <= N; i++) {
+            unf[i] = i;
+        }
+
         for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            cities[i] = new ArrayList<>();
+
             for(int j = 1; j <= N; j++) {
                 int conn = Integer.parseInt(st.nextToken());
-
-                if(cities[i].isEmpty()) {
-                    cities[i].add(i);
-                }
-
                 if(conn == 1) {
-                    cities[i].add(j);
+                    union(unf, i, j);
                 }
             }
         }
 
-        List<Integer> visitPlanCities = new ArrayList<>();
+        int[] list = new int[M + 1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++) {
-            visitPlanCities.add(Integer.parseInt(st.nextToken()));
+        for(int i = 1; i <= M; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[N + 1];
-        q.add(visitPlanCities.get(0));
-
-        while(!q.isEmpty()) {
-            int cityNum = q.poll();
-
-            for(int nextCity : cities[cityNum]) {
-                visited[cityNum] = true;
-                if(!visited[nextCity]) {
-                    q.add(nextCity);
-                }
-            }
-        }
-
-        for(int visiteCityNum : visitPlanCities) {
-            if(!visited[visiteCityNum]) {
+        for(int i = 1; i < M; i++) {
+            if(find(unf, list[i]) != find(unf, list[i + 1])) {
                 System.out.println("NO");
                 return;
             }
         }
 
         System.out.println("YES");
+    }
 
+    public static void union(int[] unf, int a, int b) {
+        int findA = find(unf, a);
+        int findB = find(unf, b);
+
+        if(findA != findB) {
+            if(a > b) {
+                unf[findB] = findA;
+            } else {
+                unf[findA] = findB;
+            }
+        }
+    }
+
+    public static int find(int[] unf, int tar) {
+        if(unf[tar] != tar) return unf[tar] = find(unf, unf[tar]);
+        else return tar;
     }
 }
